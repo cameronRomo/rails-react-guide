bcbbdd0c8fbfc59cbcf2de215ee257a45463314d
Cameron Romo <cromo1225@gmail.com>
2023-10-16 14:46:36 -0600

    Test that the recipes page loads with a given recipe

--- spec/features/recipes_page_spec.rb
+++ spec/features/recipes_page_spec.rb
@@ -7,6 +7,12 @@ feature "The recipes page", js: true do
   end
 
   scenario "renders a list of recipes" do
-    expect(page).to have_content 'My First Recipe'
+    expect(page).to have_content "My First Recipe"
+    expect(page).to have_content "View Recipe"
+  end
+
+  scenario "navigates to recipe info page after clicking view recipe button" do
+    click_on "View Recipe"
+    expect(page).to have_current_path '/recipe/1'
   end
 end
